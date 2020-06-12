86bd8b55ea85933860ff14a02af79d9c0b4cb6c0
Ayaan Siddiqui <ayaanqui@pop-os.localdomain>
2020-06-12 15:10:23 -0500

    Added User.findAll and User.findById

--- models/User.js
+++ models/User.js
@@ -1,13 +1,19 @@
 const db = require('../database');
 
-module.exports = class User {
-  static createUserTable() {
-    return db.query(`
+exports.createUserTable = () => {
+  return db.query(`
       CREATE TABLE IF NOT EXISTS users(
         id       SERIAL PRIMARY KEY,
         username VARCHAR(30) NOT NULL,
         email    VARCHAR(320) NOT NULL
       );
     `);
-  }
+};
+
+exports.findAll = () => {
+  return db.query(`SELECT * FROM users`);
+};
+
+exports.findById = id => {
+  return db.query(`SELECT * FROM users WHERE id=$1`, id);
 };
