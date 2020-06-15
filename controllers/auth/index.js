3745e01bf3bc2f7bd8a67cd85c7be3957e1f208c
ayaanqui <moahammedayaan.dev@gmail.com>
2020-06-14 19:58:08 -0500

    Added field errors

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,5 +1,9 @@
+const User = require('../../models/User');
+
 exports.auth = (req, res, next) => {
   const body = req.body;
+  const errors = [];
+
   User.findByUsername(body.username)
     .then(({ rows }) => {
       if (rows.length > 0 && body.username === rows[0].username) {
@@ -8,13 +12,17 @@ exports.auth = (req, res, next) => {
             if (rows.length > 0 && body.email === rows[0].email) {
               res.cookie('user', rows[0]).redirect('/');
             } else {
-              console.log('Incorrect email');
+              errors.push({
+                'email': 'Email is already in use by another user, please try a different one.'
+              });
               res.redirect('/');
             }
           })
           .catch(err => console.log(err));
       } else {
-        console.log('Incorrect email');
+        errors.push({
+          'username': 'Username is unavailable. Try using a different one'
+        });
         res.redirect('/');
       }
     })
