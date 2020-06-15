ad6e7596e205ff57005574327f801aa27afed274
ayaanqui <moahammedayaan.dev@gmail.com>
2020-06-14 23:57:20 -0500

    Add user if not aready in db

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,29 +1,27 @@
+const db = require('../../database');
 const User = require('../../models/User');
 
 exports.auth = (req, res, next) => {
   const body = req.body;
-  const errors = [];
 
-  User.findByUsername(body.username)
+  if (!body.username || !body.email)
+    res.redirect('/');
+
+  db.query(`SELECT * FROM users WHERE username=$1 AND email=$2`, [body.username, body.email])
     .then(({ rows }) => {
-      if (rows.length > 0 && body.username === rows[0].username) {
-        User.findByEmail(body.email)
-          .then(({ rows }) => {
-            if (rows.length > 0 && body.email === rows[0].email) {
-              res.cookie('user', rows[0]).redirect('/');
-            } else {
-              errors.push({
-                'email': 'Email is already in use by another user, please try a different one.'
-              });
-              res.redirect('/');
-            }
+      if (rows.length > 0) {
+        res.cookie('user', rows[0]).redirect('/');
+      } else {
+        User.insert(body.username, body.email).then(result => console.log(result))
+          .then(result => {
+            User.findByUsername(body.username)
+              .then(({ rows }) => {
+                console.log(rows);
+                res.cookie('user', rows[0]).redirect('/');
+              })
+              .catch(err => console.log(err));
           })
           .catch(err => console.log(err));
-      } else {
-        errors.push({
-          'username': 'Username is unavailable. Try using a different one'
-        });
-        res.redirect('/');
       }
     })
     .catch(err => console.log(err));
