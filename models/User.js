99ee0c383d04742cbfc87f8fdbd6debba975d319
ayaanqui <moahammedayaan.dev@gmail.com>
2020-06-24 11:48:52 -0500

    Redefined users table

--- models/User.js
+++ models/User.js
@@ -1,33 +1,23 @@
+const Sequelize = require('sequelize');
 const db = require('../database');
 
-const table = 'users';
+const User = db.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: Sequelize.STRING(30),
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: Sequelize.STRING(320),
+    allowNull: false,
+    unique: true,
+  },
+});
 
-exports.createTable = () => {
-  return db.query(`
-      CREATE TABLE IF NOT EXISTS ${table}(
-        id       SERIAL PRIMARY KEY,
-        username VARCHAR(30) NOT NULL UNIQUE,
-        email    VARCHAR(320) NOT NULL UNIQUE
-      );
-    `);
-};
-
-exports.findAll = () => {
-  return db.query(`SELECT * FROM ${table}`);
-};
-
-exports.findById = id => {
-  return db.query(`SELECT * FROM ${table} WHERE id=$1`, [id]);
-};
-
-exports.findByUsername = username => {
-  return db.query(`SELECT * FROM ${table} WHERE username=$1`, [username]);
-};
-
-exports.findByEmail = email => {
-  return db.query(`SELECT * FROM ${table} WHERE email=$1`, [email]);
-};
-
-exports.insert = (username, email) => {
-  return db.query(`INSERT INTO ${table} (username, email) VALUES ($1, $2)`, [username, email]);
-}
+module.exports = User;
