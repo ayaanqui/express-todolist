4510bec59e101c23cee289a749023f0c0de5a64e
ayaanqui <moahammedayaan.dev@gmail.com>
2020-06-24 12:19:34 -0500

    Using findOrCreate() to create user if does not already exist

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -7,34 +7,15 @@ exports.auth = (req, res, next) => {
   if (!body.username || !body.email)
     res.redirect('/');
 
-  db.query('SELECT * FROM users WHERE username=$1 AND email=$2', [body.username, body.email])
-    .then(({ rows }) => {
-      if (rows.length > 0) {
-        res.cookie('user', rows[0]).redirect('/');
-      } else {
-        User.findByUsername(body.username)
-          .then(usernameOb => {
-            User.findByEmail(body.email)
-              .then(emailOb => {
-                if (usernameOb.rows.length > 0 || emailOb.rows.length > 0) {
-                  console.log('Either the username or email is in use by another user...');
-                  res.redirect('/');
-                } else {
-                  // If username and email are not taken
-                  // Then we can create a new user
-                  User.insert(body.username, body.email)
-                    .then(() => {
-                      User.findByUsername(body.username)
-                        .then(({ rows }) => res.cookie('user', rows[0]).redirect('/'))
-                        .catch(err => console.log(err));
-                    })
-                    .catch(err => console.log(err));
-                }
-              })
-              .catch(err => console.log(err));
-          })
-          .catch(err => console.log(err));
+  User
+    .findOrCreate({
+      where: {
+        username: body.username,
+        email: body.email,
       }
     })
-    .catch(err => console.log(err));
+    .then(([user, created]) => {
+      res.cookie('user', user).redirect('/');
+    })
+    .catch(err => res.status(400).send({ message: 'Username or Email is already in use' }));
 };
