71392370542d07206b1a874b691b582f2054fcdb
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-05 23:40:04 -0500

    Check if user cookie exists in users table

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,28 @@
+const User = require('../models/User');
+
 exports.home = (req, res, next) => {
-  const user = (req.cookies.user) ? req.cookies.user : null;
-  const loggedIn = (user) ? true : false;
-  return res.render('pages/home/index', { loggedIn: loggedIn, user: user });
+  const localUser = (req.cookies.user) ? req.cookies.user : null;
+  const signupPage = 'pages/home/signup';
+
+  if (localUser) {
+    if (!localUser.username || !localUser.email)
+      return res.render(signupPage);
+
+    User.findOne({
+      where: {
+        username: localUser.username,
+        email: localUser.email
+      }
+    })
+      .then(user => {
+        if (!user) {
+          res.cookie('user', '', { expires: new Date(0) });
+          return res.render(signupPage);
+        }
+        return res.render('pages/home/index', { user: user });
+      })
+      .catch(err => console.log(err));
+  } else {
+    return res.render(signupPage);
+  }
 };
