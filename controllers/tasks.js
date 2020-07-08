a91d58f2197f13567318572376a8d758bb156c23
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-08 14:35:21 -0500

    Added post route to edit tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -24,6 +24,30 @@ exports.getTasks = (req, res, next) => {
 
 exports.getTask = (req, res, next) => { };
 
+exports.postEditTask = (req, res, next) => {
+  const localUser = req.cookies.user;
+
+  if (localUser) {
+    if (!localUser.username || !localUser.email)
+      return res.redirect();
+
+    User.findOne({
+      where: { username: localUser.username, email: localUser.email }
+    })
+      .then(user => {
+        const body = req.body;
+        if (body.taskId) {
+          Task.findAll({ where: { userId: user.id, id: body.taskId } })
+            .then(tasks => res.status(200).send(tasks))
+            .catch(err => console.log(err));
+        }
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.status(401).send({ 'message': 'You must be authenticated in order to create a task' });
+  }
+};
+
 exports.postAddTask = (req, res, next) => {
   const body = req.body;
   const localUser = req.cookies.user;
