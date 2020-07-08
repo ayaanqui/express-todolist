5859516283de81503124b1de1f530b5b90668b13
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-08 17:59:12 -0500

    Added support to complete task and view task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -25,7 +25,44 @@ exports.getTasks = (req, res, next) => {
   }
 };
 
-exports.getTask = (req, res, next) => { };
+exports.getTask = (req, res, next) => {
+  const localUser = req.cookies.user;
+
+  if (localUser) {
+    if (!localUser.username || !localUser.email)
+      return res.status(401).send({ 'message': 'Authentication failed' });
+
+    User.findOne({
+      where: { username: localUser.username, email: localUser.email }
+    })
+      .then(user => {
+        const taskId = req.params.taskId;
+        Task.findOne({
+          where: { userId: user.id, id: taskId }
+        })
+          .then(task => {
+            if (!task)
+              next();
+
+            const completed = req.query.completed;
+            if (completed) {
+              Task.update(
+                { completed: completed },
+                { where: { userId: user.id, id: task.id } }
+              )
+                .then(updatedTask => res.status(200).send({ id: task.id, completed: completed }))
+                .catch(err => console.log(err));
+            } else {
+              res.status(200).send(task);
+            }
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.status(401).send({ 'message': 'You must be authenticated in order to create a task' });
+  }
+};
 
 exports.postEditTask = (req, res, next) => {
   const localUser = req.cookies.user;
