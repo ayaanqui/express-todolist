7afab42bef3f38df84fcdd91b50a776f7ef03de6
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-10 17:06:05 -0500

    Ajax calls to toggle task completion

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,1 +1,21 @@
-console.log('This is working...');
+const url = 'http://localhost:3000/tasks';
+
+const toggleCompleteTask = (obj, taskId) => {
+  if ($(obj).is(":checked")) {
+    // Checked
+    $.ajax({
+      url: `${url}/${taskId}/?completed=true`,
+      success: res => {
+        $(`#task_${taskId}`).addClass('strike-through');
+      }
+    });
+  } else {
+    // Not checked
+    $.ajax({
+      url: `${url}/${taskId}/?completed=false`,
+      success: res => {
+        $(`#task_${taskId}`).removeClass('strike-through');
+      }
+    });
+  }
+};
