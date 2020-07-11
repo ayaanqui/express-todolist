d04eb14a9e717298090f08bbfaec8ace62b5451a
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-10 23:11:28 -0500

    Added logout

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ routesApp.use(require('./home'));
 routesApp.use(require('./auth'));
 routesApp.use('/tasks', require('./tasks'));
 
+routesApp.get('/logout', (req, res, next) => {
+  return res.cookie('user', '', { expires: new Date(0) }).redirect('/');
+});
+
 module.exports = routesApp;
