01dc8bb6f852cba3612b3c7d84d34501bbb68656
ayaanqui <moahammedayaan.dev@gmail.com>
2020-07-20 11:33:05 -0500

    Changed edit route to put

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,6 +7,6 @@ router.get('/', tasksController.getTasks);
 router.post('/add-task', tasksController.postAddTask);
 router.get('/:taskId', tasksController.getTask);
 router.delete('/:taskId', tasksController.deleteTask);
-router.post('/:taskId/edit', tasksController.postEditTask);
+router.put('/:taskId/edit', tasksController.postEditTask);
 
 module.exports = router;
